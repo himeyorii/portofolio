58187d97b73a81a89fb1e164f2a22c335deeaff6
himeyorii <musaaa41207@gmail.com>
2026-07-16 22:16:14 +0700

    rombak ulang porto

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
-console.log("Portfolio");
+let uang = 50000;
+const hargamouse = 15000;
+const hargakeyboard = 30000;
+const gaji_freelance = 50000;
+
+
+console.log ("Uang Awal :", uang);
+console.log("Harga Mouse :", hargamouse);
+console.log("Harga Keyboard :", hargakeyboard);
+console.log("Gaji Freelance :", gaji_freelance);
+
+
+
+uang -= hargamouse;
+
+console.log("Setelah Beli Mouse :", uang);
+
+uang -= hargakeyboard;
+
+console.log("Setelah Beli Keyboard :", uang);
+
+uang += gaji_freelance;
+
+console.log("Uang Akhir Setelah Gajian :", uang)
+
